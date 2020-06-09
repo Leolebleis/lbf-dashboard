5c75a184f4f41a804675b62d4c4e31d4c9cb3a18
leo <lelebleis@deloitte.co.uk>
2020-06-09 15:41:07 +0100

    formatting header

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -6,7 +6,7 @@ import logo from "../assets/lbf_logo.png"
 export default class Header extends React.Component {
   render() {
     return (
-      <Row className="align-items-center mt-3">
+      <Row className="align-items-center my-3">
         <Col className="col-auto">
           <img className="d-block h-auto" style={{width: "150px"}} src={logo} alt="logo LBF" />
         </Col>
